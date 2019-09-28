2a3227b502a330155e7aa0469b684b24142c9791
ouvaisaifi <54577972+ouvaisaifi@users.noreply.github.com>
2019-09-28 14:09:51 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,3 @@
 if __name__ == '__main__':
-    print('Previous - Hello World - welcome to NIIT University')
+    print('Previous - Hello World - welcome to NIIT University \n')
     print('New - Hello World - Welcome to Google Cloud')
